7fcb597d76466367c9e46a8edd4792401c279b87
kenneth27dunne <kenneth_27_dunne@msn.com>
2025-03-31 13:30:50 +0100

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://whale-app-zgtcy.ondigitalocean.app';
+export const BASE_URL = 'https://talented-splendor-1ceaa2c3d3.strapiapp.com';// 'https://whale-app-zgtcy.ondigitalocean.app';
